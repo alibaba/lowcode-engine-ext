ffdf7e155c25d1e541610ea45e92af89cbbf7e87
hzd822 <hzd822@qq.com>
2022-11-10 17:42:20 +0800

    fix: 移除codemirror

--- src/setter/style-setter/components/css-code/index.tsx
+++ src/setter/style-setter/components/css-code/index.tsx
@@ -2,21 +2,61 @@ import * as React from 'react';
 import { Button} from '@alifd/next';
 import { StyleData } from '../../utils/types';
 import { parseToCssCode, parseToStyleData } from '../../utils';
-import CodeMirror from '@uiw/react-codemirror';
-import { css } from '@codemirror/lang-css';
+import MonacoEditor from '@alilc/lowcode-plugin-base-monaco-editor';
 import Icon from '../../components/icon';
 interface CodeProps {
   styleData: StyleData | any;
   onStyleDataChange: (val: any) => void;
 }
 
-const extensions = [css()];
+
+const defaultEditorOption = {
+  readOnly: false,
+  //automaticLayout: true,
+  folding: true, // 默认开启折叠代码功能
+  wordWrap: 'off',
+  formatOnPaste: true,
+  //height:'100%',
+  fontSize: 12,
+  tabSize: 2,
+  scrollBeyondLastLine: false,
+  fixedOverflowWidgets: false,
+  snippetSuggestions: 'top',
+  minimap: {
+    enabled: false,
+  },
+  options : {
+    lineNumbers: 'off',
+    fixedOverflowWidgets:true,
+    automaticLayout:true,
+    glyphMargin: false,
+    folding: false,
+    lineDecorationsWidth: 0,
+    lineNumbersMinChars: 0
+  },
+  scrollbar: {
+    vertical: 'auto',
+    horizontal: 'auto',
+  }
+};
+
 export default class CssCode extends React.Component<CodeProps> {
   state = {
+    defaultEditorProps: {},
     cssCode: '',
     isCanSave:true,
   };
 
+  // shouldComponentUpdate(nextProps: CodeProps, nextState: any) {
+  //   if (
+  //     // nextProps.visible != this.props.visible ||
+  //     this.state.cssCode == '' && nextState.cssCode != ''
+  //   ) {
+  //     return true;
+  //   }
+
+  //   return false;
+  // }
 
   componentWillReceiveProps(nextProps: CodeProps) {
     const cssCode = parseToCssCode(nextProps.styleData);
@@ -27,22 +67,32 @@ export default class CssCode extends React.Component<CodeProps> {
 
   componentDidMount() {
     const { styleData } = this.props;
+
+    if (document.body.clientWidth >= 1860) {
+      this.setState({
+        offsetX: -400,
+      });
+    }
+
     const cssCode = parseToCssCode(styleData);
+    // console.log('cssCode', cssCode);
+
     this.setState({
       cssCode,
     });
   }
 
-
-  savaStyle = () => {
+  styleSave = () => {
+    const { cssCode } = this.state;
     const { onStyleDataChange } = this.props;
-    const {cssCode} = this.state;
     const newStyleData = parseToStyleData(cssCode);
-     // 检查是否和原来的styleData完全相同
-    newStyleData && onStyleDataChange(newStyleData);
-    this.setState({
-      isCanSave:true
-    })
+    // 检查是否和原来的styleData完全相同
+    if (newStyleData){
+      onStyleDataChange(newStyleData);
+      this.setState({
+        isCanSave:true
+      })
+    }
   }
 
 
@@ -60,23 +110,20 @@ export default class CssCode extends React.Component<CodeProps> {
   };
 
   render() {
-    const { cssCode,isCanSave } = this.state;
+    const { cssCode, defaultEditorProps, isCanSave } = this.state;
     return (
         <div>
           <div style={{marginBottom:'5px',display:'flex',alignItems:'center',justifyContent:'space-between'}}>
-            <Icon type="icon-CSS"></Icon>
-            <Button type="primary" disabled={isCanSave} onClick={this.savaStyle}>保存</Button>
-          </div>
-          <CodeMirror
-              value={cssCode}
-              onChange={(value, viewUpdate) => this.updateCode(value)}
-              extensions={extensions}
-              basicSetup={{
-                foldGutter: false,
-                lineNumbers:false
-              }}
-  
-          />
+          <Icon type="icon-CSS"></Icon>
+          <Button type="primary" onClick={this.styleSave} disabled={isCanSave}>保存</Button>
+        </div>
+          <MonacoEditor
+                value={cssCode}
+                {...defaultEditorProps}
+                {...defaultEditorOption}
+                {...{ language: 'css' }}
+                onChange={(newCode: string) => this.updateCode(newCode)}
+              />
         </div>
     );
   }
